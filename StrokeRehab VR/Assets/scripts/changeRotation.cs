dd290c9071e540066fc4fbe31bc2ebc367e484d6
tusadi <tpurang@yahoo.in>
2018-03-13 16:05:37 -0700

    16:05

---
+++ StrokeRehab VR/Assets/scripts/changeRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.rotation = Quaternion.LookRotation (gameObject.transform.forward, gameObject.transform.forward);
+	}
+}

d9c1507075f29c94114b4de15b9a048cf5d0ab0f
tusadi <tpurang@yahoo.in>
2018-03-14 13:25:45 -0700

    13:25  Sound Integration

---
+++ StrokeRehab VR/Assets/scripts/Scene1/duckSoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class duckSoundManager : MonoBehaviour {
+
+	public GameObject soundManager;
+	AudioSource source;
+
+	int curr = 0;
+
+	// Use this for initialization
+	void Start () {
+		source = gameObject.GetComponent<AudioSource> ();
+		Invoke ("playSound", Random.Range (2, 5));
+	}
+
+	void playSound(){
+		source.clip = soundManager.GetComponent<soundManager> ().clips [curr];
+		curr = (curr + 1) % 4;
+		source.PlayOneShot (source.clip);
+		Invoke ("playSound", 10);
+	}
+
+}
---
+++ StrokeRehab VR/Assets/scripts/Scene1/soundManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour {
+
+	public AudioClip[] clips;
+	public GameObject[] ducks;
+
+}

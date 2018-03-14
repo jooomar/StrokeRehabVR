47b876e44666b277123530791d4eb8527239afa4
tusadi <tpurang@yahoo.in>
2018-03-14 08:48:11 -0700

    8:47 Animations complete

--- StrokeRehab VR/Assets/scripts/DuckMovement.cs
+++ StrokeRehab VR/Assets/scripts/DuckMovement.cs
@@ -6,12 +6,20 @@ public class DuckMovement : MonoBehaviour {
 
 	Vector3 startPosition;
 	public bool Active = false;
+	public bool ret = false;
+	bool tempMovement = false;
 	public float speed = 5;
 	public Vector3 targetPosition;
 
-	public bool ret = false;
+	public Animator duckAnim;
+
+	public Vector3 tempPos;
+
+
 	void Start(){
 		startPosition = gameObject.transform.position;
+		tempPos = startPosition;
+		Invoke ("generateRandomPosition", Random.Range(1,3));
 	}
 
 	void Update(){
@@ -21,15 +29,51 @@ public class DuckMovement : MonoBehaviour {
 		} else if (ret) {
 			gameObject.transform.position = Vector3.MoveTowards (gameObject.transform.position, startPosition, Time.deltaTime * speed);
 			gameObject.transform.LookAt (startPosition);
+		} else if (tempMovement) {
+			Debug.Log ("*********************");
+			gameObject.transform.position = Vector3.MoveTowards (gameObject.transform.position, tempPos, Time.deltaTime * speed);
+			gameObject.transform.LookAt (tempPos);
 		}
+
 	}
 		
 	void OnCollisionEnter(Collision col){
 		if (col.gameObject.tag.Equals ("bread")) {
 			//set animator
+			duckAnim.SetBool("swimming", true);
 			Destroy(col.gameObject);
 			Active = false;
 			ret = true;
 		}
+
+		if (col.gameObject.tag.Equals ("start")) {
+			//set animator
+			tempMovement = true;
+			Active = false;
+			ret = false;
+			Debug.Log("dfgdfgdg");
+			duckAnim.SetBool("swimming", false);
+		}
 	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag.Equals ("start")) {
+			//set animator
+			tempMovement = true;
+			Active = false;
+			ret = false;
+			Debug.Log("dfgdfgdg");
+			duckAnim.SetBool("swimming", false);
+		}
+	}
+
+	void generateRandomPosition(){
+		int x = Random.Range (-3, 3);
+		int z = Random.Range (-3, 3);
+
+		tempPos = startPosition + new Vector3 (x, 0, z);
+		Invoke ("generateRandomPosition", Random.Range(1,3));
+
+	}
+		
 }
